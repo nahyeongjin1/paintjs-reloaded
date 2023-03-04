ac5725b0ed404dddd9e0a564d083c0344de61094
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-03-04 12:23:27 +0900

    Practice path & path.

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+
+context.rect(50, 50, 100, 100);
+context.rect(150, 150, 100, 100);
+context.rect(250, 250, 100, 100);
+context.fill();
+
+context.beginPath();
+context.rect(350, 350, 100, 100);
+context.rect(450, 450, 100, 100);
+context.fillStyle = "red";
+context.fill();
